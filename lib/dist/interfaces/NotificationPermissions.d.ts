fc67e07fd6942cc4b3dd23e0d13b254d67dcd0a4
HỒ CÔNG TRÌNH - Mobile Developer - Technology Center <trinh.ho2@mservice.com.vn>
2021-07-18 02:40:16 +0700

    #Temp build

---
+++ lib/dist/interfaces/NotificationPermissions.d.ts
@@ -0,0 +1,12 @@
+export interface NotificationPermissionOptions {
+    carPlay?: boolean;
+    criticalAlert?: boolean;
+    providesAppNotificationSettings?: boolean;
+    provisional?: boolean;
+    announcement?: boolean;
+}
+export interface NotificationPermissions extends NotificationPermissionOptions {
+    badge: boolean;
+    alert: boolean;
+    sound: boolean;
+}
